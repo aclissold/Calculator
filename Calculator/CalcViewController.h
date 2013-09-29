7fc572725d1155a63bb8f3c0cf90b3862d18ca5a
Andrew Clissold <andrewclissold@gmail.com>
2013-09-29 03:01:43 -0400

    Set up buttons; make digits functional

--- Calculator/CalcViewController.h
+++ Calculator/CalcViewController.h
@@ -10,4 +10,18 @@
 
 @interface CalcViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)clear:(id)sender;
+- (IBAction)zero:(id)sender;
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+
 @end
--- Calculator/CalcViewController.m
+++ Calculator/CalcViewController.m
@@ -26,4 +26,68 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)clear:(id)sender {
+    self.label.text = @"";
+}
+
+- (IBAction)zero:(id)sender {
+    if ([self.label.text length] != 0 && [self.label.text length] < 10) {
+        self.label.text = [self.label.text stringByAppendingString:@"0"];
+    }
+}
+
+- (IBAction)one:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"1"];
+    }
+}
+
+- (IBAction)two:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"2"];
+    }
+}
+
+- (IBAction)three:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"3"];
+    }
+}
+
+- (IBAction)four:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"4"];
+    }
+}
+
+- (IBAction)five:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"5"];
+    }
+}
+
+- (IBAction)six:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"6"];
+    }
+}
+
+- (IBAction)seven:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"7"];
+    }
+}
+
+- (IBAction)eight:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"8"];
+    }
+}
+
+- (IBAction)nine:(id)sender {
+    if ([self.label.text length] < 9) {
+        self.label.text = [self.label.text stringByAppendingString:@"9"];
+    }
+}
+
 @end
