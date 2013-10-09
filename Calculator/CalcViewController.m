9ef1f45d255f03831b65f39dfd6b55ea6bd44097
Andrew Clissold <andrewclissold@gmail.com>
2013-10-08 21:31:20 -0400

    Play keyboard click sounds on button press

--- Calculator/CalcViewController.m
+++ Calculator/CalcViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "CalcViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface CalcViewController ()
 
@@ -22,6 +23,10 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)playClick:(id)sender {
+    AudioServicesPlaySystemSound(0x450);
 }
 
 - (IBAction)clear:(id)sender {
